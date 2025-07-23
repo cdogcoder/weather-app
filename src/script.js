5812f35161ad20404b75fb42fe5ee0ac87a7708d
cdogcoder <cdogcoder@gmail.com>
2025-07-23 09:37:39 -0700

    Write function to extract weather data from API call

--- src/script.js
+++ src/script.js
@@ -6,3 +6,15 @@ async function fetchWeatherData(location) {
     console.log(json)
     return json;
 }
+
+function extractWeatherData(json) {
+    const location = json.address;
+    const currTemperature = json.currentConditions.temp;
+    const currConditions = json.currConditions.conditions;
+    const tempHigh = json.days[0].tempmax;
+    const tempLow = json.days[0].tempmin;
+    const oneHourLaterConditions = json.days[0].hours[7].conditions;
+    const twoHourLaterConditions = json.days[0].hours[8].conditions;
+    const threeHourLaterConditions = json.days[0].hours[9].conditions;
+    return [location, currTemperature, currConditions, tempHigh, tempLow, oneHourLaterConditions, twoHourLaterConditions, threeHourLaterConditions]
+}
