d25ce1aa0db6184d8cf0022b18e0d7c2e97a20aa
cdogcoder <cdogcoder@gmail.com>
2025-07-23 08:27:34 -0700

    Configure production and development Webpack scripts

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
 module.exports = {
-    mode: "development",
     entry: "./src/script.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/index.html"]
     },
     plugins: [
         new HtmlWebpackPlugin({
