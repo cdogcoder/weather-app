6ee1778d34efc4b186eea66d8a18214961fad005
cdogcoder <cdogcoder@gmail.com>
2025-07-23 09:37:14 -0700

    Write function to fetch weather data

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
 import "./styles.css";
+
+async function fetchWeatherData(location) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=3WRZYRZDUK273XPGNXVAZGD5Y&contentType=json`, {mode: "cors"});
+    const json = await response.json();
+    console.log(json)
+    return json;
+}
